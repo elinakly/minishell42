40e2aba7c395622305b75af8e8218f902acae449
elinakly <klimova.elina85@gmail.com>
2025-03-02 23:52:34 +0100

    im trying env ;(

---
+++ exec/builtins/env/env.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	env(char **envp)
+{
+	int i;
+
+	while(*envp)
+	{
+		i = 0;
+		while(envp[0][i])
+		{
+			printf("%c", envp[0][i]);
+			i++;
+		}
+		printf("\n");
+		envp++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	env(envp);
+	return (0);
+}
