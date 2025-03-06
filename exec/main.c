e56a02e09e5b7ce4ff32e61dd0c16d3e7192923e
elinakly <klimova.elina85@gmail.com>
2025-03-06 01:13:41 +0100

    readline

--- exec/main.c
+++ exec/main.c
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <linux/limits.h>
 
 int	ft_strlen(const char *s)
 {
@@ -26,35 +27,6 @@ int	ft_strlen(const char *s)
 	}
 	return (lenth);
 }
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*arr;
-	int		i;
-	int		j;
-
-	arr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		arr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		arr[i] = s2[j];
-		j++;
-		i++;
-	}
-	arr[i] = '\0';
-	return (arr);
-}
-
 
 int	ft_strncmp(char *s1, char *s2, size_t n)
 {
@@ -70,24 +42,74 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 	return (0);
 }
 
+int	ft_strlcat(char *dst, char *src, int size)
+{
+	int	dstlen;
+	int	srclen;
+	int	i;
+
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (size <= dstlen)
+		return (size + srclen);
+	i = 0;
+	while (src[i] != '\0' && (dstlen + i) < size - 1)
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (dstlen + srclen);
+}
+
+int	ft_strlcpy(char *dst, char *src, int size)
+{
+	int	i;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char *get_value(char **envp, char *key)
+{
+	int		i = 0;
+
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], key, ft_strlen(key)) == 0)
+			return (envp[i] + ft_strlen(key));
+		i++;
+	}
+	return (NULL);
+}
+
 void ft_readline(char **envp)
 {
 	int		i = 0;
 	char	*line;
-	char	*prompt = NULL;
-	char	*cwd = NULL;
+	char	prompt[PATH_MAX];
+	char	cwd[PATH_MAX];
 
-	while (envp[i])
+	char *home = get_value(envp, "HOME=");
+	int home_len = ft_strlen(home);
+	while(envp[i])
 	{
-		if (ft_strncmp(envp[i], "PWD=", 4) == 0)
-		{
-			cwd = envp[i] + 4;
-			break ;
-		}
+		ft_strlcpy(prompt, get_value(envp, "LOGNAME="), PATH_MAX);
+		ft_strlcat(prompt, "@", PATH_MAX);
+		ft_strlcat(prompt, get_value(envp, "NAME="), PATH_MAX);
+		ft_strlcat(prompt, ":~", PATH_MAX);
+		ft_strlcat(prompt, get_value(envp, "PWD=") + home_len, PATH_MAX);
+		ft_strlcat(prompt, "$", PATH_MAX);
 		i++;
 	}
-	if (cwd)
-		prompt = ft_strjoin(cwd, "/minishell$ ");
 	line = readline(prompt);
 }
 
