03b96144c0a8ce768427cd352363ca526b7891c8
Mika Schipper <mschippe@student.codam.nl>
2025-04-30 01:09:56 +0200

    add newlines to heredoc string lol

--- source/parse/heredoc/heredoc.c
+++ source/parse/heredoc/heredoc.c
@@ -1,17 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   heredoc.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mschippe <mschippe@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/29 15:08:06 by mschippe          #+#    #+#             */
-/*   Updated: 2025/04/29 15:28:31 by mschippe         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   heredoc.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mschippe <mschippe@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/04/29 15:08:06 by mschippe      #+#    #+#                 */
+/*   Updated: 2025/04/30 00:33:47 by Mika Schipp   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 #include "../../../include/builtins.h"
+
+char	*ft_strjoin_nl(const char *s1, const char *s2)
+{
+	char	*res;
+	char	*tmp;
+
+	res = ft_strjoin(s1, "\n");
+	if (!res)
+		return (NULL);
+	tmp = res;
+	res = ft_strjoin(res, s2);
+	if (!res)
+		return (NULL);
+	free(tmp);
+	return (res);
+}
 
 char	*get_heredoc(char *delim, bool expand)
 {
@@ -33,7 +49,7 @@ char	*get_heredoc(char *delim, bool expand)
 		else
 		{
 			tmp = total;
-			total = ft_strjoin(total, line);
+			total = ft_strjoin_nl(total, line);
 			free(tmp);
 		}
 	}
