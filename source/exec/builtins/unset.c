01688e4dcbe39b7dce59d0bf4fb5114c2595dd63
Mika Schipper <mschippe@student.codam.nl>
2025-05-08 15:31:24 +0200

    fix unset for elina

--- source/exec/builtins/unset.c
+++ source/exec/builtins/unset.c
@@ -5,16 +5,18 @@
 
 int		unset(t_shell *shell, t_command *cmds, char **envp)
 {
-    int     i;
+	int	i;
 
 	i = 1;
-    if (cmds->argv[1][0] == '-' && cmds->argv[1][1])
-        return (ft_putstr_fd("unset: invalid option\n", 2), 2);
-    while (i < cmds->argc)
-    {
-        if (cmds->argv[i][0] != '\0')
-            remove_env_var(&shell->venv, cmds->argv[i]);
-        i++;
-    }
-    return (0);
+	if (cmds->argc == 1)
+		return (0);
+	if (cmds->argv[1][0] == '-' && cmds->argv[1][1])
+		return (ft_putstr_fd("unset: invalid option\n", 2), 2);
+	while (i < cmds->argc)
+	{
+		if (cmds->argv[i][0] != '\0')
+			remove_env_var(&shell->venv, cmds->argv[i]);
+		i++;
+	}
+	return (0);
 }
